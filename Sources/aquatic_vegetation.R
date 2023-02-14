b416d10c1ddd4d258d5457d4ec1d0777c7841d7c
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2023-02-14 17:07:42 +0100

    Code R

---
+++ Sources/aquatic_vegetation.R
@@ -0,0 +1,8 @@
+cat("\014") # Clear console
+rm(list=ls())# Clean workspace
+
+set.seed(2023)  #Set seed for reproducibility
+
+#===============================================================
+#Install packages
+
