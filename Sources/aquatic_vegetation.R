bce8ebbd0bbb65fefa2f265371f0b043d62e5a83
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2023-02-14 18:12:33 +0100

    Test modification of function

--- Sources/aquatic_vegetation.R
+++ Sources/aquatic_vegetation.R
@@ -21,7 +21,7 @@ setwd(dir.repository)
 library(stringr)
 
 # Initialization
-log_extraction = T 
+log_extraction = T
 data_bareme_extraction = 'jeu_de_donnes_jaugeages'    # jeu_de_donnes_jaugeages or jeu_de_donnes_courbes_correction
 
 # Load functions
@@ -35,8 +35,18 @@ station_extraction <- file.path(dir.repository,dir.data,dir.cvl,dir.jaugeages)  
 #===============================================================
 
 if(log_extraction==T){
-  extraction(dir_data_ext=station_extraction,
-             data_exraction=data_bareme_extraction)
+  extraction_data <- extraction(dir_data_ext=station_extraction,
+                                data_exraction=data_bareme_extraction)
+  stations_info_jaugeages <- extraction_data[[1]]
+  Dh_station_info_jau <-   extraction_data[[2]]
+  nom_station <-   extraction_data[[3]]
+  
+}else{   # Only for CVL stations!!
+  load(file = file.path(station_extraction,data_bareme_extraction,'database_to_explore.RData')) #Dh_station_info_jau
+  load(file = file.path(station_extraction,data_bareme_extraction,'nom_station_organisation.RData')) #nom_station
+  load(file = file.path(station_extraction,data_bareme_extraction,'database.RData')) #stations_info_jaugeages
 }
+
+stations_info_jaugeages[[1]]
 
 
