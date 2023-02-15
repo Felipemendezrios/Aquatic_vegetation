24f705fe3d230ff13e6af86252095b2550ee6919
Felipe MENDEZ RIOS <108263997+Felipemendezrios@users.noreply.github.com>
2023-02-15 09:51:58 +0100

    Tests and develop routine to plot

--- Sources/aquatic_vegetation.R
+++ Sources/aquatic_vegetation.R
@@ -19,16 +19,17 @@ setwd(dir.repository)
 #===============================================================
 # Libraries:
 library(stringr)
+library(ggplot2)
 
 # Initialization
 log_extraction = F
-data_bareme_extraction = 'jeu_de_donnes_jaugeages'    # jeu_de_donnes_jaugeages or jeu_de_donnes_courbes_correction
+data_bareme_extraction = 'jeu_de_donnes_courbes_correction'    # jeu_de_donnes_jaugeages or jeu_de_donnes_courbes_correction
 
 # Load functions
 source(file=file.path(dir.repository,dir.source,'Functions.R'))
 
 # File path 
-station_extraction <- file.path(dir.repository,dir.data,dir.cvl,dir.jaugeages)    #dir.jaugeages or dir.courbe_corr
+station_extraction <- file.path(dir.repository,dir.data,dir.cvl,dir.courbe_corr)    #dir.jaugeages or dir.courbe_corr
 
 #===============================================================
 # script to read information of gauging of Bareme
@@ -37,16 +38,34 @@ station_extraction <- file.path(dir.repository,dir.data,dir.cvl,dir.jaugeages)  
 if(log_extraction==T){
   extraction_data <- extraction(dir_data_ext=station_extraction,
                                 data_exraction=data_bareme_extraction)
-  stations_info_jaugeages <- extraction_data[[1]]
-  Dh_station_info_jau <-   extraction_data[[2]]
+  stations_info <- extraction_data[[1]]
+  Dh_station_info <-   extraction_data[[2]]
   nom_station <-   extraction_data[[3]]
   
 }else{   # Only for CVL stations!!
-  load(file = file.path(station_extraction,data_bareme_extraction,'database_to_explore.RData')) #Dh_station_info_jau
+  load(file = file.path(station_extraction,data_bareme_extraction,'database_to_explore.RData')) #Dh_station_info
   load(file = file.path(station_extraction,data_bareme_extraction,'nom_station_organisation.RData')) #nom_station
-  load(file = file.path(station_extraction,data_bareme_extraction,'database.RData')) #stations_info_jaugeages
+  load(file = file.path(station_extraction,data_bareme_extraction,'database.RData')) #stations_info
 }
 
-Dh_station_info_jau[[1]]
+stations_info_complet <- NULL
+Dh_station_info_complet <- NULL
+for(i in 1:length(Dh_station_info)){
+  stations_info[[i]]$Date=as.POSIXct(stations_info[[i]]$Date,format="%d/%m/%Y %H:%M",tz = "GMT")
+  Dh_station_info[[i]]$Date=as.POSIXct(Dh_station_info[[i]]$Date,format="%d/%m/%Y %H:%M",tz = "GMT")
+  
+  stations_info_complet <- rbind(stations_info_complet,stations_info[[i]])
+  Dh_station_info_complet <- rbind(Dh_station_info_complet,Dh_station_info[[i]])
+}
 
-
+  ggplot(Dh_station_info_complet,aes(Date,`Dh(cm)`))+
+    geom_point(aes(colour=factor(id)),alpha=0.5)+
+    labs(title='Points de correction',
+         x='Date (année)',
+         y='Dh (cm)',
+         colour='Station')+
+    theme_bw()+
+    scale_y_reverse()+
+    theme(plot.title = element_text(size=16,
+                                    hjust=0.5),
+          )
